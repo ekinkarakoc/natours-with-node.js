4faaad95d7b50d9a38b7683567b1b5fa22fb38f4
Ekin KARAKOÇ <ekinkarakoc@imecar.local>
2024-07-18 14:55:01 +0300

    training crud operators

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.json());
 //     .send({ message: 'Hello from the server side1', app: 'Nautors' });
 // });
 
+/* ------------ GET REQUEST --------------  */
 const tours = JSON.parse(fs.readFileSync(`./dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
@@ -29,18 +30,22 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params);
+/* ------------ GET REQUEST(id) --------------  */
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id == id);
+
+  console.log(tour);
   res.status(200).send({
     status: 'success',
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
+    data: {
+      tour,
+    },
   });
 });
 
+/* ------------ POST REQUEST --------------  */
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours.length;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -60,6 +65,33 @@ app.post('/api/v1/tours', (req, res) => {
     }
   );
 });
+
+/* ------------ UPDATE REQUEST (PATCH) --------------  */
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: '<Updated tour here....>',
+    },
+  });
+});
+
+/* ------------ DELETE REQUEST --------------  */
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  res.status(200).json({
+    success: 'BAŞARILIIIIII',
+  });
+});
+/* ------------ LISTENING PORT --------------  */
 
 const port = 3006;
 app.listen(port, () => {
