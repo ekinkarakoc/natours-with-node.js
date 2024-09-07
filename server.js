a0902741acb3be46afbf20c5f6e06306145e968c
ekinkarakoc <ekinkarakoc1@gmail.com>
2024-09-07 22:15:00 +0300

    deneme commiti

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Server Listening on port ${port}`);
+  console.log(`Server Listening on port deneme ${port}`);
 });
